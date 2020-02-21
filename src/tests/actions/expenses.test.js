7b3c11c43f276dad76ed9fef7cbcf0f0b37567a3
Jack <n8886997@qut.edu.au>
2020-02-21 17:00:34 +1000

    added startEditExpebse async

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,7 +2,8 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {startAddExpense, 
   addExpense, 
-  editExpense, 
+  editExpense,
+  startEditExpense, 
   removeExpense, 
   setExpenses, 
   startSetExpenses, 
@@ -59,6 +60,26 @@ test('should setup edit expense action object', () => {
     })
 
 })
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = {amount: 21045};
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type:'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
+  });
+
+})
+
 
 test('should setup add expense action object with provided vals', () => {
 
