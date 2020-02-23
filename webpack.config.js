c885e60802418582dabf77f0a03c3c837afee6b8
Jack <n8886997@qut.edu.au>
2020-02-23 14:26:34 +1000

    updated webpack config for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env) => {
     const CSSExctract = new ExtractTextPlugin('styles.css');
     console.log('env', env)
     return {
-      entry: ['babel-polyfill',"./src/app.js"]
+      entry: ['babel-polyfill',"./src/app.js"],
       output: {
         path: path.join(__dirname, "public", "dist"),
         filename: "bundle.js"
